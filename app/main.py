e3c0a6106cb54a4717bf96d184856f4d853b7c28
Cade <river1988@mail.ru>
2025-05-25 21:28:29 +0400

    Добавлена проверка наличия бд и учетной записи админа при инициализации

--- app/main.py
+++ app/main.py
@@ -1,10 +1,21 @@
+from contextlib import asynccontextmanager
+from typing import AsyncGenerator
+
 from fastapi import FastAPI
+
+from app.initial_data import init_db
 from app.projects.router import router as router_projects
 from app.tasks.router import router as router_tasks
 from app.comments.router import router as router_comments
 from app.users.router import router as router_users
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(_: FastAPI) -> AsyncGenerator[None, None]:
+    await init_db()
+    yield
+
+app = FastAPI(lifespan=lifespan)
+
 
 app.include_router(router_tasks)
 app.include_router(router_projects)
