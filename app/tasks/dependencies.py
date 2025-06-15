0ac8b827fd59d3e58ce13449ce995744a85e94ec
KinCadeFoster <river1988@mail.ru>
2025-06-16 02:41:26 +0400

    - рефакторинг для задач

--- app/tasks/dependencies.py
+++ app/tasks/dependencies.py
@@ -1,16 +1,14 @@
-from fastapi import Depends, HTTPException
-from app.users.dependencies import get_current_user
 from app.projects.service import ProjectService
-from app.exceptions import UserPermissionError
+from app.exceptions import UserPermissionError, UserIsNotMemberProject
 from app.users.models import UsersTableModel
 
-async def check_user_can_access_task_for_manager_or_user_tasks(
+async def check_access_for_tasks(
     project_id,
-    current_user: UsersTableModel = Depends(get_current_user)
+    current_user: UsersTableModel
 ):
     if not (current_user.is_manager or current_user.is_user):
         raise UserPermissionError
     in_project = await ProjectService.user_in_project(project_id, current_user.id)
     if not in_project:
-        raise HTTPException(status_code=403, detail="User not in project")
+        raise UserIsNotMemberProject
     return current_user
--- app/tasks/router.py
+++ app/tasks/router.py
@@ -1,8 +1,5 @@
-from fastapi import APIRouter, HTTPException, status, Depends
+from fastapi import APIRouter, status, Depends
 
-from app.comments.dependencies import check_access_for_comments
-from app.projects.service import ProjectService
-from app.tasks.dependencies import check_user_can_access_task_for_manager_or_user_tasks
 from app.tasks.schemas import SchemaTask, SchemaTaskAdd, SchemaTaskUpdate
 from app.tasks.service import TaskService
 from app.users.dependencies import get_current_user
@@ -10,53 +7,23 @@ from app.users.models import UsersTableModel
 
 router = APIRouter(prefix="/tasks",tags=["Tasks"])
 
-@router.get("/")
-async def get_all_tasks() -> list[SchemaTask]:
-    return await TaskService.find_all()
 
 @router.get("/{task_id}", response_model=SchemaTask)
 async def get_task_by_id(task_id:int, current_user: UsersTableModel = Depends(get_current_user)):
-    await check_access_for_comments(task_id, current_user)
-    task = await TaskService.find_by_id(task_id)
-    if not task:
-        raise HTTPException(status_code=404, detail="Task not found")
-    return task
+    return await TaskService.get_task_by_id(task_id, current_user)
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=SchemaTask)
 async def add_task(new_task: SchemaTaskAdd, current_user: UsersTableModel = Depends(get_current_user)):
-    await check_user_can_access_task_for_manager_or_user_tasks(new_task.project_id, current_user)
-    project = await ProjectService.find_by_id(new_task.project_id)
-    if not project:
-        raise HTTPException(status_code=404, detail="Project not found")
-    new_task_dict = new_task.model_dump()
-    new_task_dict["creator_id"] = current_user.id
-    new_task_dict["status"] = 1
-    new_task_dict["local_task_id"] = 1
-    return await TaskService.add(**new_task_dict)
+    return await TaskService.add_task(new_task, current_user)
 
 @router.patch("/{task_id}", response_model=SchemaTask)
 async def update_task(task_id: int, data: SchemaTaskUpdate, current_user: UsersTableModel = Depends(get_current_user)):
-    await check_access_for_comments(task_id, current_user)
-    task = await TaskService.update_by_id(task_id, **data.model_dump())
-    if not task:
-        raise HTTPException(status_code=404, detail="Task not found")
-    return task
+    return await TaskService.update_task(task_id, data, current_user)
 
 @router.delete("/{task_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_task(task_id: int, current_user: UsersTableModel = Depends(get_current_user)):
-    await check_access_for_comments(task_id, current_user)
-    task = await TaskService.find_by_id(task_id)
-    if not task:
-        raise HTTPException(status_code=404, detail="Task not found")
-    result = await TaskService.delete_by_id(task_id)
-    if not result:
-        raise HTTPException(status_code=404, detail="Task not found")
-    return None
+    return await TaskService.delete_task(task_id, current_user)
 
 @router.get("/tasks-by-project/{project_id}", response_model=list[SchemaTask])
 async def get_tasks_by_project(project_id: int, current_user: UsersTableModel = Depends(get_current_user)):
-    await check_user_can_access_task_for_manager_or_user_tasks(project_id, current_user)
-    project = await ProjectService.find_by_id(project_id)
-    if not project:
-        raise HTTPException(status_code=404, detail="Project not found")
-    return await TaskService.find_all(project_id=project_id)
+    return await TaskService.get_tasks_by_project(project_id, current_user)
