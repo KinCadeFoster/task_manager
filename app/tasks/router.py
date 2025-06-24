c81e49e22a27686a8ba0a1f5a8518b5beae4aeac
KinCadeFoster <river1988@mail.ru>
2025-06-24 05:54:51 +0400

    - refactoring

--- app/tasks/router.py
+++ app/tasks/router.py
@@ -34,4 +34,4 @@ async def get_tasks_by_project(
         local_task_id: int,
         current_user: UsersTableModel = Depends(get_current_user)
 ):
-    return await TaskService.get_tasks_by_prefix_and_id(project_prefix, local_task_id, current_user)
+    return await TaskService.get_task_by_prefix_and_id(project_prefix, local_task_id, current_user)
